e0c617082dd44912d7a37e0866a0d8a0ede60ae7
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-05-27 17:56:38 +0530

    commit 6
    added faculty page with css and JS

---
+++ faculty/faculty.js
@@ -0,0 +1,17 @@
+// JavaScript code for modifying the page dynamically
+// Example: Change the header background color on mouseover
+var header = document.getElementById('idh1');
+
+header.addEventListener('mouseover', function () {
+    header.style.backgroundColor = getRandomColor();
+});
+
+// Function to generate a random color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
