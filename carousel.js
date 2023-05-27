9b693504ad5157bc95132a1ae3a3a0415ccf019c
MSH <84580747+MsHegde-code@users.noreply.github.com>
2023-05-27 23:20:20 +0530

    carousel is added to index

---
+++ carousel.js
@@ -0,0 +1,18 @@
+const carousel = document.querySelector('.carousel-items');
+const slides = Array.from(document.querySelectorAll('.item'));
+let currentIndex = 0;
+const slideWidth = slides[0].clientWidth;
+let intervalId;
+
+function startAutoplay() {
+  intervalId = setInterval(() => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    carousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+  }, 2000); // Adjust the interval duration (in milliseconds) as desired
+}
+
+function stopAutoplay() {
+  clearInterval(intervalId);
+}
+
+startAutoplay();
