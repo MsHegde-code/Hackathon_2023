354683d81dbfdf0904c9aa74b05692172bb3d11b
Maithreya-100 <maithreya2002@gmail.com>
2023-05-27 14:01:31 +0530

    commit 3

---
+++ dept/ece/ece.js
@@ -0,0 +1,17 @@
+// JavaScript code for modifying the page dynamically
+// Example: Change the header background color on mouseover
+var header = document.querySelector('header');
+
+header.addEventListener('mouseover', function() {
+  header.style.backgroundColor = getRandomColor();
+});
+
+// Function to generate a random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 10)];
+  }
+  return color;
+}
